b7681c97d5c31100897ce4605cf87ed914c5b459
Augusto Monteiro <augustoengenharia@outlook.com>
2023-05-21 18:38:41 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroLojas/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CadastroLojas.Models
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto>options):base(options)
+        {
+
+        }
+        public DbSet<LojasModel> Lojas { get; set; }
+    }
+}
---
+++ CadastroLojas/Models/LojasModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CadastroLojas.Models
+{
+    [Table("Catalogo de Lojas")]
+    public class LojasModel
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Estado")]
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+
+        [Column("Cidade")]
+        [Display(Name = "Cidade")]
+        public string Cidade { get; set; }
+
+        [Column("Unidade")]
+        [Display(Name = "Unidade")]
+        public string Unidade { get; set; }
+    }
+}
